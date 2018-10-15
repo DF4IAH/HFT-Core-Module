5a58b9bc548d8f26a407a0d9ca62a1b5bde8fc5e
Ulrich Habel <espero7757@gmx.net>
2018-10-16 00:20:32 +0200

    SW - timer - hunting for PCLK bug

--- SW/HFT-Core-Module_TrueSTUDIO/patch_ALL.sh
+++ SW/HFT-Core-Module_TrueSTUDIO/patch_ALL.sh
@@ -3,4 +3,5 @@
 patch -R -p2 < patch-R_CoreSrcSai.diff
 patch -R -p2 < patch-R_CoreIncStm32l4xx_hal_conf.diff
 patch -R -p2 < patch-R_DriversSrcStmSrcHalRcc.diff
+patch -R -p2 < patch-R_CoreSrcTim.diff
 
